51c8ba419d65b2ee989f0a0ef12f4abcd4e11769
jgpicos <jpicos@ucsc.edu>
2021-04-11 18:57:44 -0700

    Time constraints enabled

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -89,15 +89,33 @@ class Play extends Phaser.Scene {
             fixedWidth: 100
         }
         this.scoreLeft = this.add.text(borderUISize + borderPadding, borderUISize + borderPadding*2, this.p1Score, scoreConfig);
+
+        // GAME OVER flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update() {
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+            this.scene.restart();
+        }
+        
         this.starfield.tilePositionX -= 4;
 
-        this.p1Rocket.update();
-        this.ship1.update();
-        this.ship2.update();
-        this.ship3.update();
+        if (!this.gameOver) {               
+            this.p1Rocket.update();         // update rocket sprite
+            this.ship1.update();           // update spaceships (x3)
+            this.ship2.update();
+            this.ship3.update();
+        } 
 
         if(this.checkCollision(this.p1Rocket, this.ship1)) {
             this.p1Rocket.alpha = 0
